7c5b2695f278ff6ad7295664d69fe5a27cf696e4
Taslim Dosunmu <tjdosunmu@gmail.com>
2019-10-23 23:44:54 -0700

    use functions to capture database query operations

--- backend/index.js
+++ backend/index.js
@@ -3,16 +3,26 @@ const path = require('path');
 // Installed dependencies
 const express = require('express');
 
+const {
+    getAnswers,
+    getQuestions,
+} = require('./queries');
+
 const app = express();
 
-app.get('/questions', (req, res) => {
+app.get('/questions', async (req, res) => {
     // get a random question
-    res.json('What is a question?');
+    res.json(await getQuestions());
 });
 
-app.get('/answers/:questionId', (req, res) => {
+app.get('/answers/:questionId', async (req, res) => {
     // get the answers associated with a question
-    res.json(['correct answer', 'incorrect answer 1', 'incorrect answer 2']);
+    try {
+        const qId = parseInt(req.params.questionId, 10);
+        res.json(await getAnswers(qId));
+    } catch (err) {
+        res.status(400).json('Invalid value provided for questionId -- it must be an int');
+    }
 });
 
 app.get('*', (req, res) => {
